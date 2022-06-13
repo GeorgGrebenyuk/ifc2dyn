b6b96d15b4f7b205dd556454d87a0b7db3e429ef
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-06-13 22:57:34 +0300

    Add temporal debug console app

---
+++ src/debug_app/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ifc2dyn;
+namespace debug_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IfcDoc test1 = new IfcDoc(@"E:\DataTest\Renga\BridgeEx.ifc");
+            string units = test1.LengthUnits();
+
+            Console.WriteLine(units);
+
+            Console.ReadKey();
+        }
+    }
+}
