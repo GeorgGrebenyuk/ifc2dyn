9865dcdbaea5385a58fff0c409c6bbc4c908282b
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-06-13 22:58:09 +0300

    Create class for getting IfcElement data

---
+++ src/ifc2dyn/IfcElementInfo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+using dg = Autodesk.DesignScript.Geometry;
+using dr = Autodesk.DesignScript.Runtime;
+using GeometryGym.Ifc;
+using static ifc2dyn.IfcDoc;
+
+namespace ifc2dyn
+{
+    public class IfcElementInfo
+    {
+        private IfcElement element;
+        [dr.MultiReturn("Description", "GlobalId", "Guid", "Index", "Name", "ObjectPlacement", 
+            "ObjectType", "Representation", "StepClassName","Tag")]
+        public Dictionary<string,object> get_info()
+        {
+            return new Dictionary<string, object>()
+            {
+                {"Description",element.Description },
+                {"GlobalId",element.GlobalId },
+                {"Guid",element.Guid.ToString() },
+                {"Index",element.Index },
+                {"Name",element.Name },
+                {"ObjectPlacement",element.ObjectPlacement },
+                {"ObjectType",element.ObjectType },
+                {"Representation",element.Representation },
+                {"StepClassName",element.StepClassName },
+                {"Tag",element.Tag }
+            };
+        }
+        public IfcElementInfo (IfcElement input_element)
+        {
+            this.element = input_element;
+        }
+    }
+}
